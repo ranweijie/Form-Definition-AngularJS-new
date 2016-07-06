f0102c8024bd2bb3945f7f4aebd522f4cfbaa733
ranweijie <ranweijie@ranweijie.com>
2016-07-06 14:32:52 +0800

    use  to delete form item

--- js/myForm.js
+++ js/myForm.js
@@ -2,11 +2,10 @@
  * Created by ranweijie on 16-7-5.
  */
 angular.module('myForm',['ngRoute','myForm.controllers'])
-    // .run(["$rootScope",function ($rootScope) {
     .run(["$rootScope",function ($rootScope) {
         $rootScope.items = []
     }])
-    .config(['$routeProvider',"$locationProvider" ,function($routeProvider,$locationProvider){
+    .config(['$routeProvider',function($routeProvider){
         $routeProvider
         .when('/edit',{
             templateUrl:"tpl/edit.html",
@@ -17,15 +16,13 @@ angular.module('myForm',['ngRoute','myForm.controllers'])
         }).otherwise({
             redirectTo:'/edit'
         });
-        // $locationProvider.html5Mode(true);
     }]);
 
 
 
 angular.module('myForm.controllers',[])
+
     .controller("editPageController",['$rootScope','$location',"$scope",function($location,$scope,$rootScope){
-        // $scope.statusButtonTitle = "预览";
-        // $scope.hasPopUpBox = false;
 
         $rootScope.statusButtonTitle = "预览";
         $rootScope.hasPopUpBox = false;
@@ -42,30 +39,24 @@ angular.module('myForm.controllers',[])
             // console.log("itemType==>",itemType);
             if(!itemType){
                 alert("Sorry,您还没有选择要添加的类型！！");
-                return
+                return false
             }
             var addItemId = parseInt((new Date()).getTime()),
                 addItemText = itemType == "text" ? "我是文本"+addItemId :"我是日期"+addItemId;
                 addItem = {"type":itemType,"id":addItemId,"text":addItemText};
-
             // console.log(addItem);
             $rootScope.items.push(addItem);
         };
-        $rootScope.removeItem = function (id) {
-            for(var i in $rootScope.items){
-                if($rootScope.items[i]["id"] == id){
-                    $rootScope.items.splice(i,1);
-                    break;
-                }
-            }
+        $rootScope.removeItem = function (index) {
+            $rootScope.items.splice(index,1);
         }
-
     }])
+
     .controller("previewPageController",['$location',"$rootScope","$scope",function($location,$rootScope,$scope){
         $scope.statusButtonTitle = "编辑" ;
 
         $scope.submitForm = function () {
             // alert($rootScope.items);
-            window.alert("Sorry 提交功能不完善，暂不可提交！")
+            window.alert("Sorry,提交功能不完善，暂不可提交！")
         };
     }]);
